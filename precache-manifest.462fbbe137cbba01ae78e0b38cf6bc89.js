b32fc192c23b3fe9fe7cd8bb438c7bd92f7f3979
classic-creator <himangshukhound322@gmail.com>
2022-09-04 21:59:37 +0530

    Updates

---
+++ precache-manifest.462fbbe137cbba01ae78e0b38cf6bc89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d32ca8f0674185ef6f80",
+    "url": "/text-editor/static/css/main.2ec17c28.chunk.css"
+  },
+  {
+    "revision": "d32ca8f0674185ef6f80",
+    "url": "/text-editor/static/js/main.d32ca8f0.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/text-editor/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "847c6e58d319653692c4",
+    "url": "/text-editor/static/js/2.847c6e58.chunk.js"
+  },
+  {
+    "revision": "d7c0f36315f620921fbb",
+    "url": "/text-editor/static/js/runtime~main.d7c0f363.js"
+  },
+  {
+    "revision": "a702bc6a820d60df28c6f784d9a8c82e",
+    "url": "/text-editor/index.html"
+  }
+];
